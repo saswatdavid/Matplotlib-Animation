b62245fde5058f03eeb4128c3c7c8207bb89e8ba
Saswat David <72136339+saswatdavid-pharmeasy@users.noreply.github.com>
2021-09-29 04:01:14 +0530

    Basic

---
+++ Pandas-Tricks.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#Create Sample Data
+df = pd.DataFrame({'name': ['Sam', 'Mike', 'Lisa'], 'items': [24, 44, 55]})
+df.head()
+
+#Counting Number Of Values
+df['name'].value_counts()
+
+#Querying Data
+df.query('name == "Sam"')
+df.query('items >= 20')
