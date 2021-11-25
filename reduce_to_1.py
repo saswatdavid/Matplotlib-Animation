74a28f214d7af64cef12d1da7ed415f0136fbe49
Saswat David <72136339+saswatdavid-pharmeasy@users.noreply.github.com>
2021-11-26 02:56:34 +0530

    2 Sols

--- reduce_to_1.py
+++ reduce_to_1.py
@@ -2,8 +2,7 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def solution(n):
-    # Your code here
+def solution_1(n):
     n = int(n)
     count = 0
     while n!= 1:
@@ -24,7 +23,7 @@ def solution_2(n):
     count = 0
     while n > 1:
         if n % 2 == 0:
-           n = n/2
+            n = n/2
         else:
             if (n == 3 or n % 4 == 1):
                 n = (n - 1)
@@ -33,11 +32,30 @@ def solution_2(n):
         count += 1
     return count
 
+def solution_3(n):
+    n = int(n)
+    count = 0
+    while n!= 1:
+        if n%2!=0:
+            if (math.log((n+1),2))%1==0 and n!=3:
+                n = n+1
+                count += 1
+                print(n)
+            else:
+                n = n-1
+                count += 1
+                print(n)
+        else:
+            n = n/2
+            count += 1
+            print(n)
+    return count
+
 rows=[]
-for i in range(1,20):
-    rows.append([i, solution(i)])
+for i in range(1,1000):
+    rows.append([i, solution_2(i), solution_3(i)])
 
-df = pd.DataFrame(rows, columns=["A", "B"])
+df = pd.DataFrame(rows, columns=["A","B","C"])
 
-df.plot(x="A", y="B")
+df.plot(x="A", y=["B","C"])
 plt.show()
